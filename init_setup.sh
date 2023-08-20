8de7d04bf1bb31a1a910fa545d574a631546b283
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-08-19 23:30:01 -0700

    updated

--- init_setup.sh
+++ init_setup.sh
@@ -4,5 +4,5 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
 source activate ./env
 echo [$(date)]: "Installing dev requirements"
-pip install -r reuirements_dev.txt
+pip install -r requirements_dev.txt
 echo [$(date)]: "START"
